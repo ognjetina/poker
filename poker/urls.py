38a9e11c4e3b015817d4579c4a56e1ae1403823e
anitejngo <laogdo@gmail.com>
2019-03-20 21:33:28 +0100

    Init commit

---
+++ poker/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from notifier.views import HomeView
+
+urlpatterns = [
+    path('', HomeView.as_view()),
+    path('admin/', admin.site.urls),
+]
